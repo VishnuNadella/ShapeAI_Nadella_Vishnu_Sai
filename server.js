fe162cf0f87747ee772c4a4f65c8919a633ea6e1
VishnuNadella <63490737+VishnuNadella@users.noreply.github.com>
2021-05-17 15:41:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}));
-app.get("/", function (request, resp){
-    resp.sendFile(__dirname + "/index.html");
+
+app.get("/", function (req, res){
+    res.sendFile(__dirname + "/index.html");
 });
 
-// app.get("/about", function(request, resp){
-//     resp.send("<h1>This is Vishnu Nadella</h1>")
-// });
-
-
-app.post("/", function(request, resp){
-    console.log(request.body);
-    var n1 = Number(request.body.num1);
-    var n2 = Number(request.body.num2);
+app.post("/", function(req, res){
+    var n1 = Number(req.body.num1);
+    var n2 = Number(req.body.num2);
     var result = n1 + n2;
-    resp.send("The answer is " + result)
+    res.send("The answer is " + result);
 });
 
 app.listen(3000, function(){
-    console.log("Server has started at local host port 3000")
+    console.log("Server has started on port 3000")
 });
